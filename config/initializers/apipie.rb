67a327f4047580aac71c24e28db72e931494eef8
raj209 <30597777+raj209@users.noreply.github.com>
2018-04-03 09:35:15 -0500

    Updating API Version

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,7 +2,9 @@ Apipie.configure do |config|
   config.app_name                = "popHealth"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
-  config.default_version         = "5.0"
+  config.default_version         = "5.1"
+  config.translate               = false
+  config.default_locale          = nil
   config.api_controllers_matcher = ["#{Rails.root}/app/controllers/api/*.rb", "#{Rails.root}/app/controllers/api/admin/*.rb"]
   config.app_info                = <<-EOS
   API documentation for popHealth. This API is used by the web front end of popHealth but
